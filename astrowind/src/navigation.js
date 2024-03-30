25a1f6811d02ccf57ef390f62cadbfe2f6e7381a
Stephen Grice <stephengrice@live.com>
2024-03-30 16:31:04 -0400

    Add products pages

--- astrowind/src/navigation.js
+++ astrowind/src/navigation.js
@@ -3,112 +3,41 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      text: 'Home',
+      href: '/',
     },
     {
-      text: 'Pages',
+      text: 'Products',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: '💣 Boom Languages',
+          href: getPermalink('/products/boom'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: '✅ KeyDo',
+          href: getPermalink('/products/keydo'),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: '📙 KeyReader',
+          href: getPermalink('/products/keyreader'),
         },
         {
-          text: 'About us',
-          href: getPermalink('/about'),
+          text: '💻 PageKey Operating System',
+          href: getPermalink('/products/pkos'),
         },
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
+          text: '🎥 PKVid',
+          href: getPermalink('/products/pkvid'),
         },
         {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
+          text: '🎹 Plaintext DAW',
+          href: getPermalink('/products/ptd'),
         },
       ],
     },
     {
       text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
+      href: '/blog',
     },
     {
       text: 'Team',
